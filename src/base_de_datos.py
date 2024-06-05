5f372e782fa73d7b28e4a7c9783e4791b5985401
ecorralro <ecorralr689@alumnos.imf.com>
2024-06-05 19:09:36 +0100

    creación de tablas SQL

--- src/base_de_datos.py
+++ src/base_de_datos.py
@@ -29,3 +29,44 @@ def crear_tabla_usuarios():
     );
     '''
     ejecutar_consulta(consulta)
+# Crear el resto de tablas adicionales
+def crear_tablas_adicionales():
+    consulta_vinos = '''
+    CREATE TABLE IF NOT EXISTS vinos (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        nombre TEXT,
+        bodega TEXT,
+        ano INTEGER,
+        tipo_uva TEXT,
+        denominacion_origen TEXT,
+        precio REAL
+    );
+    '''
+    ejecutar_consulta(consulta_vinos)
+
+    consulta_opiniones = '''
+    CREATE TABLE IF NOT EXISTS opiniones (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        usuario_id INTEGER,
+        vino_id INTEGER,
+        opinion TEXT,
+        FOREIGN KEY(usuario_id) REFERENCES usuarios(id),
+        FOREIGN KEY(vino_id) REFERENCES vinos(id)
+    );
+    '''
+    ejecutar_consulta(consulta_opiniones)
+
+    consulta_experiencias = '''
+    CREATE TABLE IF NOT EXISTS experiencias (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        usuario_id INTEGER,
+        vino_id INTEGER,
+        fecha TEXT,
+        contexto TEXT,
+        maridaje TEXT,
+        companeros TEXT,
+        FOREIGN KEY(usuario_id) REFERENCES usuarios(id),
+        FOREIGN KEY(vino_id) REFERENCES vinos(id)
+    );
+    '''
+    ejecutar_consulta(consulta_experiencias)
